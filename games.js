099ac61ad27204696752e0cf3aaa38a4e6ca6745
stevegkim13 <stevegkim13@gmail.com>
2021-10-04 21:07:33 -0700

    Added: keypress function and nextSequence updates (level-change on h1).

--- games.js
+++ games.js
@@ -4,6 +4,28 @@ var gamePattern = [];
 
 var userClickedPattern = [];
 
+var started = false;
+
+var level = 0;
+
+
+
+$(document).keypress(function() {
+
+  if (!started) {
+
+    $("#level-title").text("Level " + level);
+
+    nextSequence();
+
+    started = true;
+
+    }
+
+});
+
+
+
 $(".btn").click(function() {
 
   var userChosenColour = $(this).attr("id");
@@ -16,7 +38,13 @@ $(".btn").click(function() {
 
 });
 
+
+
 function nextSequence() {
+
+  level++;
+
+  $("#level-title").text("Level " + level);
 
   var randomNumber = Math.floor(Math.random()* 4);
 
@@ -30,19 +58,26 @@ function nextSequence() {
 
 }
 
+
+
 function playSound(name) {
 
   var audio = new Audio ("sounds/" + name + ".mp3")
+
   audio.play();
 
 }
+
+
 
 function animatePress(currentColour) {
 
   $("#" + currentColour).addClass("pressed");
 
   setTimeout(function() {
+
     $("#" + currentColour).removeClass("pressed");
-  }, 100);
+
+    }, 100);
 
 }
