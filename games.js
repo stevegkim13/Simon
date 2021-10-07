705a7eb53495318fe86488d395424168c536f65c
stevegkim13 <stevegkim13@gmail.com>
2021-10-06 19:56:17 -0700

    Added: function checkAnswer, startover to finish.

--- games.js
+++ games.js
@@ -36,14 +36,50 @@ $(".btn").click(function() {
 
   animatePress(userChosenColour);
 
+  checkAnswer(userClickedPattern.length - 1);
+
 });
+
+
+
+function checkAnswer (currentLevel) {
+
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+
+  if (userClickedPattern.length === gamePattern.length) {
+
+    setTimeout(function() {
+      nextSequence();
+    }, 1000);
+
+  }
+
+  } else {
+
+    var wrong = new Audio ("sounds/wrong.mp3")
+    wrong.play();
+
+    $("body").addClass("game-over");
+
+    setTimeout(function() {
+      $("body").removeClass("game-over");
+    }, 200);
+
+    $("#level-title").text("Game Over! Press Any Key to Restart");
+
+    startOver();
+
+  }
+
+}
 
 
 
 function nextSequence() {
 
-  level++;
+  userClickedPattern = [];
 
+  level++;
   $("#level-title").text("Level " + level);
 
   var randomNumber = Math.floor(Math.random()* 4);
@@ -54,7 +90,7 @@ function nextSequence() {
 
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
-  playSound(userChosenColour);
+  playSound(randomChosenColour);
 
 }
 
@@ -74,10 +110,18 @@ function animatePress(currentColour) {
 
   $("#" + currentColour).addClass("pressed");
 
-  setTimeout(function() {
-
-    $("#" + currentColour).removeClass("pressed");
-
+    setTimeout(function() {
+      $("#" + currentColour).removeClass("pressed");
     }, 100);
+
+}
+
+
+
+function startOver() {
+
+  level = 0;
+  gamePattern = [];
+  started = false;
 
 }
